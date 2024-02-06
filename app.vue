854e2750662255c2cfbff095745c06b252d99ba6
Dzakwan Diego <diegodzakwan@gmail.com>
2024-02-06 19:38:54 +0700

    [Update] Layout: add layout

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <Button>clickme</Button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/sidebar.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div class="w-1/5 h-full bg-slate-500">
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
