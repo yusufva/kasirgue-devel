df85e23eacc19c0eaddce2c1fa24af8e08b17270
unknown <diegodzakwan@gmail.com>
2024-02-09 00:21:40 +0700

    [feat] components: add header and layout for pages

---
+++ components/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="w-full h-20">
+            Header
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="flex gap-4">
-      <Sidebar />
-    <div>
-      <slot />
+  <div class="flex gap-4 pr-4 pb-4 w-screen h-screen">
+    <Sidebar />
+    <div class="flex flex-col w-full">
+      <Header />
+      <div class="w-full h-full p-4 rounded-lg bg-black/5">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Header from "@/components/header.vue";
 import Sidebar from "@/components/sidebar.vue";
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    Header,
   },
 };
 </script>
