7212c7201617761f42514aab4c1d254b8c32ea87
unknown <diegodzakwan@gmail.com>
2024-03-22 15:19:24 +0700

    [feat] penjualan: add daftar penjualan page

---
+++ pages/penjualan/[id].vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <div v-if="loading" class="flex flex-col w-full h-max gap-4">
+      <svg
+        class="mx-auto animate-[pulse_0.75s_infinite] h-10 w-10 rounded-full bg-primary"
+        viewBox="0 0 24 24"
+      ></svg>
+    </div>
+    <div v-else class="flex flex-col w-full h-max gap-4">
+      <!-- header -->
+      <div class="flex gap-1 text-xs">
+        <NuxtLink to="/pembelian" class="text-black/60 hover:text-primary"
+          >Pembelian
+        </NuxtLink>
+        <div class="text-primary capitalize">/ {{ dataBarang.id }}</div>
+      </div>
+      <div class="text-primary font-semibold text-2xl">Detail Pembelian</div>
+      <div class="h-[2px] w-full bg-primary/20 rounded-xl"></div>
+      <!-- content -->
+      <div class="flex flex-col w-full gap-4">
+        <!-- title -->
+        <Table class="w-1/2">
+          <TableBody>
+            <TableRow class="border-none">
+              <TableCell class="pb-0">ID</TableCell>
+              <TableCell class="pb-0">: {{ dataBarang.id }}</TableCell>
+            </TableRow>
+            <TableRow class="border-none">
+              <TableCell>Tanggal</TableCell>
+              <TableCell>: {{ useFormat.dateFormat(dataBarang.date) }}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <!-- items -->
+        <Table>
+          <TableHeader class="border-b border-black text-primary">
+            <TableHead>Barang</TableHead>
+            <TableHead>Jumlah</TableHead>
+            <TableHead>Harga</TableHead>
+            <TableHead>Total Harga</TableHead>
+          </TableHeader>
+          <TableBody class="border-b border-black/10 capitalize">
+            <TableRow v-for="item in detailBarang" :key="item.index">
+              <TableCell>{{ item.name }}</TableCell>
+              <TableCell>{{ item.quantity }}</TableCell>
+              <TableCell>{{ useFormat.currencyFormat(item.buying_price) }}</TableCell>
+              <TableCell>{{ useFormat.currencyFormat(item.total_price) }}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Table class="w-full text-right">
+          <TableBody>
+            <TableRow>
+              <TableCell class="font-semibold"
+                >Total Pembelian: {{ useFormat.currencyFormat(dataBarang.final_price) }}</TableCell
+              >
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { useUseFormat } from "@/stores/useFormat";
+import { useEnvStore } from "@/stores/envStore";
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@/components/ui/table";
+export default {
+  setup() {
+    const useFormat = useUseFormat();
+    return { useFormat };
+  },
+  components: {
+    Table,
+    TableHead,
+    TableHeader,
+    TableBody,
+    TableCell,
+    TableRow,
+  },
+  data() {
+    return {
+      dataBarang: [],
+      detailBarang: [],
+      loading: true,
+    };
+  },
+  methods: {
+    async getDataBarang() {
+      try {
+        const barang = await axios.get(
+          useEnvStore().apiUrl + "/api/tx-sell/" + this.$route.params.id
+        );
+        this.dataBarang = barang.data.data;
+        this.detailBarang = barang.data.data.items;
+        console.log(this.dataBarang);
+        this.loading = false;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.getDataBarang();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/penjualan/index.vue
+++ pages/penjualan/index.vue
@@ -1,9 +1,104 @@
 <template>
-  <div>Penjualan</div>
+  <div>
+    <!-- header -->
+    <div class="text-primary font-semibold text-2xl">Daftar Transaksi Penjualan</div>
+    <div class="h-[2px] w-full bg-primary/20 rounded-xl my-4"></div>
+    <!-- content -->
+    <Table>
+      <TableHeader class="text-primary font-poppins">
+        <TableRow>
+          <TableHead>ID Transaksi</TableHead>
+          <TableHead>Tanggal</TableHead>
+          <TableHead>Total Harga</TableHead>
+          <TableHead class="text-right"></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody v-if="loading">
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell>
+          <svg
+            class="mx-auto mt-10 animate-[pulse_0.75s_infinite] h-10 w-10 rounded-full bg-primary"
+            viewBox="0 0 24 24"
+          ></svg>
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+      </TableBody>
+      <TableBody v-else>
+        <TableRow
+          v-for="item in jualList"
+          :key="item.index"
+          class="border-b border-black/10"
+        >
+          <TableCell>{{ item.id }}</TableCell>
+          <TableCell>{{ useFormat.dateFormat(item.date) }}</TableCell>
+          <TableCell>{{ useFormat.currencyFormat(item.final_price) }}</TableCell>
+          <TableCell>
+            <NuxtLink class="flex items-center text-primary text-xs gap-1" :to="/penjualan/+item.id">
+              <ArrowTopRightOnSquareIcon class="w-5"/>Detail
+            </NuxtLink>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
 </template>
 
 <script>
-export default {};
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@heroicons/vue/24/outline";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/vue/24/solid";
+import { useEnvStore } from "@/stores/envStore";
+import { useUseFormat } from "@/stores/useFormat";
+import axios from "axios";
+export default {
+  setup(){
+    const useFormat = useUseFormat()
+    return {useFormat}
+  },
+  components: {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+    Button,
+    PlusIcon,
+    ArrowTopRightOnSquareIcon
+  },
+  data() {
+    return {
+      loading: true,
+      jualList: [],
+    };
+  },
+  methods: {
+    async getJualList() {
+      try {
+        const beli = await axios.get(useEnvStore().apiUrl + "/api/tx-sell");
+        this.jualList = beli.data.data;
+        console.log(this.jualList);
+        this.loading = false;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.getJualList();
+  },
+};
 </script>
-
-<style lang="scss" scoped></style>
